278237f9934a6c6e7f265fa640a05145162a72b6
JK999 <wlsrud7307@gmail.com>
2024-10-07 20:23:43 +0900

    modified:   mysite/db.sqlite3 	modified:   mysite/polls/__pycache__/models.cpython-310.pyc 	modified:   mysite/polls/models.py

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField(auto_now_add=True) # auto_now_add 퀘스찬이 처음 생길때 시간을 add한다.
     # 모델의 변경사항
     # is_something = models.BooleanField(default=False)
     # average_score = models.FloatField(default=0.0)
