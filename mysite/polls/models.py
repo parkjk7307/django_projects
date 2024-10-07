f1fe98702ceee6a08ce14672363bc983ef5633ea
JK999 <wlsrud7307@gmail.com>
2024-10-07 15:17:48 +0900

    modified:   mysite/db.sqlite3 	modified:   mysite/polls/__pycache__/models.cpython-310.pyc 	new file:   mysite/polls/migrations/__pycache__/0002_question_average_score_question_is_something.cpython-310.pyc 	modified:   mysite/polls/models.py 	new file:   mysite/sqldiff.exe 	new file:   mysite/sqlite3.exe 	new file:   mysite/sqlite3_analyzer.exe

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -13,7 +13,9 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-
+    # 모델의 변경사항
+    # is_something = models.BooleanField(default=False)
+    # average_score = models.FloatField(default=0.0)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
