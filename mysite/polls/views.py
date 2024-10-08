6e71d41c0a9cb7d5bf7175fc253cac896e1a1c9b
JK999 <wlsrud7307@gmail.com>
2024-10-08 15:48:13 +0900

    modified:   mysite/polls/__pycache__/views.cpython-310.pyc 	new file:   mysite/polls/templates/polls/index.html 	modified:   mysite/polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
+from .models import *
+from django.shortcuts import render
 
-def index(requset):
-    return HttpResponse("Hello, world.")
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'first_question': latest_question_list[0]}
+    return render(request, 'polls/index.html', context)
 
-def some_url(requset):
+def some_url(request):
     return HttpResponse("some url를 구현해 봤습니다.")
