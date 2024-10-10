a7f82a3171d8710e8c1d7cd9aacb459349c0ae04
JK999 <wlsrud7307@gmail.com>
2024-10-11 01:04:57 +0900

    modified:   mysite/polls/__pycache__/urls.cpython-310.pyc 	modified:   mysite/polls/__pycache__/views.cpython-310.pyc 	modified:   mysite/polls/templates/polls/index.html 	modified:   mysite/polls/urls.py 	modified:   mysite/polls/views.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'questions'
 urlpatterns = [
     #path('admin/', admin.site.urls),# 어드민이라는 url이 들어오면 여기로 연결해라
     path('',views.index, name='index'),
     path('some_url',views.some_url),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='question_detail'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import *
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+ 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -9,7 +10,13 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    
+    # #에러의 처리
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
     #return HttpResponse(f"입력받은 id: {question_id}")
 
