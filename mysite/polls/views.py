fb37a2313af1ffb10c40d3bee0a8022ea1b2b961
JK999 <wlsrud7307@gmail.com>
2024-10-10 20:48:18 +0900

    modified:   mysite/polls/__pycache__/views.cpython-310.pyc 	new file:   mysite/polls/templates/polls/detail.html 	modified:   mysite/polls/views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -9,9 +9,9 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    #question = Question.objects.get(pk=question_id)
-    #return render(request, 'polls/detail.html', {'question': question})
-    return HttpResponse(f"입력받은 id: {question_id}")
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse(f"입력받은 id: {question_id}")
 
 def some_url(request):
     return HttpResponse("some url를 구현해 봤습니다.")
