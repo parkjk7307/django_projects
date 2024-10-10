27c64d6b37b3c2d1f0c50b0889db48ff794abb0b
JK999 <wlsrud7307@gmail.com>
2024-10-10 20:35:57 +0900

    modified:   mysite/polls/__pycache__/urls.cpython-310.pyc 	modified:   mysite/polls/__pycache__/views.cpython-310.pyc 	modified:   mysite/polls/urls.py 	modified:   mysite/polls/views.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     #path('admin/', admin.site.urls),# 어드민이라는 url이 들어오면 여기로 연결해라
     path('',views.index, name='index'),
     path('some_url',views.some_url),
+    path('<int:question_id>/', views.detail, name='detail'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,5 +8,10 @@ def index(request):
     # context = {'first_question': latest_question_list[0]}
     return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    #question = Question.objects.get(pk=question_id)
+    #return render(request, 'polls/detail.html', {'question': question})
+    return HttpResponse(f"입력받은 id: {question_id}")
+
 def some_url(request):
     return HttpResponse("some url를 구현해 봤습니다.")
