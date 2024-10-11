8919d0f011c603788d5d83d0c89bcb932c715a5f
JK999 <wlsrud7307@gmail.com>
2024-10-11 13:11:42 +0900

    modified:   mysite/db.sqlite3 	modified:   mysite/polls/__pycache__/urls.cpython-310.pyc 	modified:   mysite/polls/__pycache__/views.cpython-310.pyc 	new file:   mysite/polls/templates/polls/result.html 	modified:   mysite/polls/urls.py 	modified:   mysite/polls/views.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -12,5 +12,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'), 
     path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),  
+    path('<int:question_id>/vote/', views.vote, name='vote'), 
+    path('<int:question_id>/result/', views.result, name='result'), 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect #, Http404
 from .models import *
 from django.shortcuts import render, get_object_or_404
 from django.urls import * 
+from django.db.models import F
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -29,6 +30,13 @@ def vote(request, question_id):
         #return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다.'})
         return render(request, 'polls/detail.html', {'question': question, 'error_message': f"선택이 없습니다. id={request.POST['choice']}"})
     else:
-        selected_choice.votes += 1
+        # A서버에서도 Votes = 1
+        # B서버에서도 Votes = 1 
+        selected_choice.votes = F('votes') + 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:index'))
+        return HttpResponseRedirect(reverse('polls/result.html', args=(question.id,)))
+    
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
