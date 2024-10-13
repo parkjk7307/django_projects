4c51cba1ddbe94efc0374f1e71ea6180d45da2f7
JK999 <wlsrud7307@gmail.com>
2024-10-13 14:43:41 +0900

    modified:   mysite/db.sqlite3 	modified:   mysite/polls_api/__pycache__/serializers.cpython-310.pyc 	modified:   mysite/polls_api/serializers.py

--- mysite/polls_api/serializers.py
+++ mysite/polls_api/serializers.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers
 from polls.models import Question
 
-class QuestionSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    question_text = serializers.CharField(max_length=200)
-    pub_date = serializers.DateTimeField(read_only=True)
-
-    def create(self, validated_data):
-        return Question.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.question_text = validated_data.get('question_text', instance.question_text) + '[시리얼라이저에서 업데이트]' # instance.question_text 값이 없을때 원래값을 유지하라란 의미
-        instance.save()
-        return instance
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id','question_text', 'pub_date']
