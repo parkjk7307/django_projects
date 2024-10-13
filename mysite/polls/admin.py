c2185a46d9b7b63511bd233457ec9b9830e10455
JK999 <wlsrud7307@gmail.com>
2024-10-13 11:17:20 +0900

    modified:   mysite/db.sqlite3 	modified:   mysite/polls/__pycache__/admin.cpython-310.pyc 	modified:   mysite/polls/__pycache__/models.cpython-310.pyc 	modified:   mysite/polls/admin.py

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Question)
+
 admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        ('Question', {'fields': ['question_text']}),
+        ('Create_Date', {'fields': ['pub_date'], 'classes': ['collapse']}),        
+    ]
+    readonly_fields = ['pub_date'] # model.py에 적혀있는 에러 해결법 auto_now_add=True
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
